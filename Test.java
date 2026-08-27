9bbe62db7778bea078a6af1a7ad283b5299829f4
Your GitHub Username <mubashireengaffarmiya@gmail.com>
2026-08-27 10:05:08 +0530

    Your commit message

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        int a=10;
+        float b=3.14f;
+        float c=a*b;
+        System.out.println("the value of c :" +c+ "end");
+    }
+    
+}
---
+++ test.py
@@ -0,0 +1,16 @@
+# user_info.py
+
+# 1. Print a welcome message
+print("Welcome to the Python Program!")
+
+# 2. Ask for the user's name
+name = input("Enter your name: ")
+
+# 3. Ask for the user's age
+age = int(input("Enter your age: "))
+
+# 4. Print a personalized greeting
+print("Hello", name, "! You are", age, "years old.")
+
+# 5. Show a simple calculation
+print("In 5 years, you will be", age + 5, "years old.")
